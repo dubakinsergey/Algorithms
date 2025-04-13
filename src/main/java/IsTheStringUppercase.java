0ef7ac823c269a47c4e64d67817a5530c6b38ea1
Sergey Dubakin <dubakinsergey@gmail.com>
2025-04-13 17:53:55 +0500

    IsTheStringUppercase_commit

---
+++ src/main/java/IsTheStringUppercase.java
@@ -0,0 +1,35 @@
+/*
+Строка заглавная?
+
+Задача
+Создайте метод, который проверяет, состоит ли строка ВСЕХ ЗАГЛАВНЫХ букв.
+
+Примеры (вход -> выход)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+*/
+
+public class IsTheStringUppercase {
+
+    public static boolean isUpperCase(String s) {
+
+        for (char el : s.toCharArray()
+        ) {
+            if (Character.isLowerCase(el)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUpperCase("c"));
+        System.out.println(isUpperCase("C"));
+        System.out.println(isUpperCase("hello I AM DONALD"));
+        System.out.println(isUpperCase("HELLO I AM DONALD"));
+        System.out.println(isUpperCase("ACSKLDFJSgSKLDFJSKLDFJ"));
+        System.out.println(isUpperCase("ACSKLDFJSGSKLDFJSKLDFJ"));
+    }
+}
