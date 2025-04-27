f3cc88589162d7c6e5423fe10137b96d55d62b1b
Sergey Dubakin <dubakinsergey@gmail.com>
2025-04-27 17:23:55 +0500

    YouCantCodeUnderPressure_commit

---
+++ src/main/java/YouCantCodeUnderPressure.java
@@ -0,0 +1,16 @@
+/*
+Вы не можете писать код под давлением #1
+
+Кодируйте как можно быстрее!Вам нужно удвоить целое число и вернуть его.
+*/
+
+public class YouCantCodeUnderPressure {
+
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(2));
+    }
+}
