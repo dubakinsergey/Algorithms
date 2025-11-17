127969adf1c0f86c6d3c797fa54afa96c630a76c
Sergey Dubakin <dubakinsergey@gmail.com>
2025-11-17 20:29:10 +0500

    Core_ListTasks_add_25_UpperCase

--- src/main/java/core/ListTasks.java
+++ src/main/java/core/ListTasks.java
@@ -9,95 +9,95 @@ import java.util.stream.Collectors;
 public class ListTasks {
 
     // Задача 1: Проверить, что список не пустой
-    public static boolean checkListIsEmpty1(List<String> list) {
+    public static boolean checkListIsEmpty_1(List<String> list) {
         return list.isEmpty();
     }
 
     // Задача 2: Проверить, что размер списка равен 5
-    public static boolean checkListIsSize2(int number, List<String> list) {
+    public static boolean checkListIsSize_2(int number, List<String> list) {
         if (list.size() == number) return true;
         else return false;
     }
 
     // Задача 3: Проверить, что список содержит строку "Cherry"
-    public static boolean checkContainsString3(String string, List<String> list) {
+    public static boolean checkContainsString_3(String string, List<String> list) {
         return list.contains(string);
     }
 
     // Задача 4: Проверить, что список НЕ содержит строку "Grape"
-    public static boolean checkNotContainsString4(String string, List<String> list) {
+    public static boolean checkNotContainsString_4(String string, List<String> list) {
         return (!list.contains(string));
     }
 
     // Задача 5: Проверить, что первый элемент списка равен "Apple"
-    public static boolean checkFirstElement5(String string, List<String> list) {
+    public static boolean checkFirstElement_5(String string, List<String> list) {
         return list.get(0).equals(string);
     }
 
     // Задача 6: Проверить, что последний элемент списка равен "Date"
-    public static boolean checkLastElement6(String string, List<String> list) {
+    public static boolean checkLastElement_6(String string, List<String> list) {
         return list.get(list.size() - 1).equals(string);
     }
 
     // Задача 7: Найти индекс первого вхождения "Banana" (ожидается 1)
-    public static int findIndexOfFirstOccurrence7(String string, List<String> list) {
+    public static int findIndexOfFirstOccurrence_7(String string, List<String> list) {
         return list.indexOf(string);
     }
 
     // Задача 8: Найти индекс последнего вхождения "Banana" (ожидается 3)
-    public static int findIndexOfLastOccurrence8(String string, List<String> list) {
+    public static int findIndexOfLastOccurrence_8(String string, List<String> list) {
         return list.lastIndexOf(string);
     }
 
     // Задача 9: Добавить элемент "Elderberry" в конец списка
-    public static List<String> addElementToTheEndList9(String string, List<String> list) {
+    public static List<String> addElementToTheEndList_9(String string, List<String> list) {
         list.add(string);
         return list;
     }
 
     // Задача 10: Вставить элемент "Apricot" на позицию с индексом 1
-    public static List<String> addElementToFirstIndex10(String string, List<String> list) {
+    public static List<String> addElementToFirstIndex_10(String string, List<String> list) {
         list.add(1, string); //Элементы с этого индекса и дальше сдвигаются вправо + Размер списка увеличивается на 1
         return list;
     }
 
     // Задача 11: Заменить элемент по индексу '{index}' на '{string}'
-    public static List<String> replaceElementToIndex11(int index, String string, List<String> list) {
+    public static List<String> replaceElementToIndex_11(int index, String string, List<String> list) {
         list.set(index, string);
         return list;
     }
 
     // Задача 12: Удалить элемент "Banana" (первое вхождение)
-    public static List<String> deleteElementToFirstOccurrence12(String string, List<String> list) {
+    public static List<String> deleteElementToFirstOccurrence_12(String string, List<String> list) {
         list.remove(string); //Элементы справа от удаленного сдвигаются влево + Размер списка уменьшается на 1
         return list;
     }
 
     // Задача 13: Удалить элемент по индексу '{index}'
-    public static List<String> deleteElementByIndex13(int index, List<String> list) {
+    public static List<String> deleteElementByIndex_13(int index, List<String> list) {
         list.remove(index);
         return list;
     }
 
     // Задача 14: Очистить список и проверить, что он пуст
-    public static boolean clearListAndCheckIsEmpty14(List<String> list) {
+    public static boolean clearListAndCheckIsEmpty_14(List<String> list) {
         list.clear();
         return list.isEmpty();
     }
 
     // Задача 15: Создать новый список из элементов с индексами от 1 до 3 (включительно)
-    public static List<String> getSubListByIndexRange15(int fromIndex, int toIndex, List<String> list) {
+    public static List<String> getSubListByIndexRange_15(int fromIndex, int toIndex, List<String> list) {
         List<String> newSublist = list.subList(fromIndex, toIndex);
         return newSublist;
     }
 
     // Задача 16: Проверить, что этот новый список содержит ["Banana", "Cherry", "Banana"]
-    public static boolean checkSubListContent16(List<String> list, int fromIndex, int toIndex, List<String> expected) {
+    public static boolean checkSubListContent_16(List<String> list, int fromIndex, int toIndex, List<String> expected) {
         return list.subList(fromIndex, toIndex).equals(expected);
     }
 
     // Задача 17: Удалить элементы с индексами от 1 до 3 из исходного списка
-    public static List<String> removeSublistByIndexRange17(int fromIndex, int toIndex, List<String> list) {
+    public static List<String> removeSublistByIndexRange_17(int fromIndex, int toIndex, List<String> list) {
         list.subList(fromIndex, toIndex).clear();
         return list;
     }
@@ -110,7 +110,7 @@ public class ListTasks {
         поэтому любые изменения в подсписке отражаются на оригинальном списке
         (в данном случае - на copy).
     */
-    public static List<String> removeSublistByIndexRangeSaveOriginal17(int fromIndex, int toIndex, List<String> originalList) {
+    public static List<String> removeSublistByIndexRangeSaveOriginal_17(int fromIndex, int toIndex, List<String> originalList) {
         List<String> copy = new ArrayList<>(originalList);
         List<String> sublist = copy.subList(fromIndex, toIndex);
         sublist.clear();
@@ -118,7 +118,7 @@ public class ListTasks {
     }
 
     // Задача 18: Напечатать все элементы списка в консоль (через for-each)
-    public static List<String> printAndReturnList18(List<String> list) {
+    public static List<String> printAndReturnList_18(List<String> list) {
         for (String element : list) {
             System.out.println(element);
         }
@@ -126,7 +126,7 @@ public class ListTasks {
     }
 
     // Задача 18: Напечатать все элементы списка в консоль (через for) с нумерацией (только печать)
-    public static void printAllElements18(List<String> list) {
+    public static void printAllElements_18(List<String> list) {
 
         if (list == null || list.isEmpty()) {
             System.out.println("List is empty");
@@ -139,7 +139,7 @@ public class ListTasks {
     }
 
     // Задача 19: Напечатать все элементы, которые начинаются с буквы 'B'
-    public static void printElementStartWithLetterB19(List<String> list) {
+    public static void printElementStartWithLetterB_19(List<String> list) {
 
         if (list == null || list.isEmpty()) {      //Проверка на null и пустой список
             System.out.println("List is empty");
@@ -167,7 +167,7 @@ public class ListTasks {
             3. Прямая проверка на null
             .filter(element -> element != null)
     */
-    public static void printElementStartWithLetterBStream19(List<String> list) {
+    public static void printElementStartWithLetterBStream_19(List<String> list) {
 
         if (list == null || list.isEmpty()) {
             System.out.println("List is empty");
@@ -182,7 +182,7 @@ public class ListTasks {
     /*
      .allMatch() для пустого stream возвращает true
     */
-    public static boolean allElementsLongerThanThree20(List<String> list) {
+    public static boolean allElementsLongerThanThree_20(List<String> list) {
 
         if (list == null || list.isEmpty()) {
             System.out.println("List is empty");
@@ -197,7 +197,7 @@ public class ListTasks {
     /*
       .anyMatch() останавливается при нахождении первого подходящего элемента
     */
-    public static boolean oneElementHasLength5Characters21(List<String> list) {
+    public static boolean oneElementHasLength5Characters_21(List<String> list) {
 
         if (list == null || list.isEmpty()) {
             System.out.println("List is empty");
@@ -208,7 +208,7 @@ public class ListTasks {
     }
 
     // Задача 22: Найти первый элемент, который начинается на "C", или вернуть "Not Found"
-    public static String firstElementStartsWithC22(List<String> list) {
+    public static String firstElementStartsWithC_22(List<String> list) {
 
         if (list == null || list.isEmpty()) {
             return "Not Found";
@@ -222,7 +222,7 @@ public class ListTasks {
     }
 
     // Задача 23: Преобразовать List<String> в List<Integer>
-    public static List<Integer> convertStringListToIntegerList23(List<String> stringNumbers) {
+    public static List<Integer> convertStringListToIntegerList_23(List<String> stringNumbers) {
 
         if (stringNumbers == null || stringNumbers.isEmpty()) {
             System.out.println("List is invalid");
@@ -282,6 +282,20 @@ public class ListTasks {
                 .toList();                           // неизменяемый список
     }
 
+    // Задача 25: Преобразовать все элементы списка в верхний регистр
+    public static List<String> listUpperCase_25(List<String> list) {
+
+        if (list == null || list.isEmpty()) {
+            System.out.println("List is empty");
+            return new ArrayList<>();
+        }
+
+        return list.stream()
+                .filter(Objects::nonNull)
+                .map(String::toUpperCase) // эквивалентно .map(el -> el.toUpperCase())
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
 
         List<String> stringNumbers = Arrays.asList("1", "2", "3", "4", "5");
@@ -295,34 +309,34 @@ public class ListTasks {
 
         list.forEach(System.out::println);
         System.out.println("------------------------");
-        System.out.println(checkListIsEmpty1(list));
-        System.out.println(checkListIsSize2(4, list));
-        System.out.println(checkContainsString3("Cherry", list));
-        System.out.println(checkNotContainsString4("Grape", list));
-        System.out.println(checkFirstElement5("Apple", list));
-        System.out.println(checkLastElement6("Date", list));
-        System.out.println(findIndexOfFirstOccurrence7("Banana", list));
-        System.out.println(findIndexOfLastOccurrence8("Banana", list));
-        System.out.println(addElementToTheEndList9("Elderberry", list));
-        System.out.println(addElementToFirstIndex10("Apricot", list));
-        System.out.println(replaceElementToIndex11(2, "Cranberry", list));
-        System.out.println(deleteElementToFirstOccurrence12("Banana", list));
-        System.out.println(deleteElementByIndex13(0, list));
-        System.out.println(clearListAndCheckIsEmpty14(list));
-        System.out.println(getSubListByIndexRange15(1, 4, Arrays.asList("Apple", "Banana", "Cherry", "Banana", "Date")));
-        System.out.println(checkSubListContent16(list, 1, 4, Arrays.asList("Banana", "Cherry", "Banana")));
-        System.out.println(removeSublistByIndexRange17(1, 4, list));
-        System.out.println(removeSublistByIndexRangeSaveOriginal17(1, 4, list));
-        System.out.println(printAndReturnList18(list));
-        printAllElements18(list);
-        printElementStartWithLetterB19(list);
-        printElementStartWithLetterBStream19(list);
-        System.out.println(allElementsLongerThanThree20(list));
-        System.out.println(oneElementHasLength5Characters21(list));
-        System.out.println(firstElementStartsWithC22(list));
-        System.out.println(convertStringListToIntegerList23(stringNumbers));
+        System.out.println(checkListIsEmpty_1(list));
+        System.out.println(checkListIsSize_2(4, list));
+        System.out.println(checkContainsString_3("Cherry", list));
+        System.out.println(checkNotContainsString_4("Grape", list));
+        System.out.println(checkFirstElement_5("Apple", list));
+        System.out.println(checkLastElement_6("Date", list));
+        System.out.println(findIndexOfFirstOccurrence_7("Banana", list));
+        System.out.println(findIndexOfLastOccurrence_8("Banana", list));
+        System.out.println(addElementToTheEndList_9("Elderberry", list));
+        System.out.println(addElementToFirstIndex_10("Apricot", list));
+        System.out.println(replaceElementToIndex_11(2, "Cranberry", list));
+        System.out.println(deleteElementToFirstOccurrence_12("Banana", list));
+        System.out.println(deleteElementByIndex_13(0, list));
+        System.out.println(clearListAndCheckIsEmpty_14(list));
+        System.out.println(getSubListByIndexRange_15(1, 4, Arrays.asList("Apple", "Banana", "Cherry", "Banana", "Date")));
+        System.out.println(checkSubListContent_16(list, 1, 4, Arrays.asList("Banana", "Cherry", "Banana")));
+        System.out.println(removeSublistByIndexRange_17(1, 4, list));
+        System.out.println(removeSublistByIndexRangeSaveOriginal_17(1, 4, list));
+        System.out.println(printAndReturnList_18(list));
+        printAllElements_18(list);
+        printElementStartWithLetterB_19(list);
+        printElementStartWithLetterBStream_19(list);
+        System.out.println(allElementsLongerThanThree_20(list));
+        System.out.println(oneElementHasLength5Characters_21(list));
+        System.out.println(firstElementStartsWithC_22(list));
+        System.out.println(convertStringListToIntegerList_23(stringNumbers));
         System.out.println(eachElementAddSuffix_fruit_immutable_24(list));
         System.out.println(eachElementAddSuffix_fruit_24(list));
+        System.out.println(listUpperCase_25(list));
     }
 }
-// Задача 25: Преобразовать все элементы списка в верхний регистр
